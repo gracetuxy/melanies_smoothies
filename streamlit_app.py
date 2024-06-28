ee2103f09f53d9a95f07fa8e22fdf4e1f068bb59
Grace Tu <88392014+gracetuxy@users.noreply.github.com>
2024-06-28 11:32:59 -0600

    Update streamlit_app.py
    use search_on field and removed extra spaces in my_insert_stmt

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,13 +40,13 @@ if ingredients_list: #is not null
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-                    values ('""" + ingredients_string + """ ',' """ + name_on_order + """')"""
+                    values ('""" + ingredients_string + """','""" + name_on_order + """')"""
     
-    # st.write(my_insert_stmt)
+    st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
     
     if time_to_insert:
